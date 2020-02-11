4f97beb2a5bf8071eaa6e529ec84d557f6f3a1b8
Mel <54912463+mel-f-dev@users.noreply.github.com>
2020-02-11 17:31:07 +0900

    file_upload project

---
+++ file_upload/fileup/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from fileup import views
+
+app_name = 'fileup'
+
+urlpatterns=[
+    path('register', views.PostRegisterView.as_view(), name='register'),
+    path('list', views.PostList.as_view(), name='list'),
+    path('<int:pk>detail', views.PostDetailView.as_view(), name='detail'),
+]
---
+++ file_upload/fileup/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import FormView, ListView, DetailView
+
+from django.urls import reverse_lazy
+
+from fileup.forms import PostRegisterForm
+from fileup.models import Post
+
+# Create your views here.
+
+
+# 게시물 등록 - 폼/처리
+class PostRegisterView(FormView):
+    template_name = 'fileup/register_form.html'   # 등록 폼 템플릿
+    form_class = PostRegisterForm
+    success_url = reverse_lazy('fileup:list')    # 등록 처리 후 이동할 페이지
+
+    def form_valid(self, form):
+        cleaned_data = form.cleaned_data    # 검증이 끝난 데이터
+        # 요청파라미터 조회 -> models.Post 저장 -> insert
+        title = cleaned_data.get('title')
+        info = cleaned_data.get('info')
+        picture = cleaned_data.get('picture')
+        file = cleaned_data.get('file')
+        post = Post(title=title, info=info, picture=picture, file=file)
+        post.save()    # insert - 업로드 된 파일이 지정된 디렉토리에 저장된다.
+        return super().form_valid(form)
+
+class PostList(ListView):
+    model = Post
+    template_name = 'fileup/list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'fileup/detail.html'
